65248ce2fb4e2edf5a6cf650fb7e680ca3066262
Wasambo <erick@afb.com>
2015-06-21 01:15:54 +0300

    further config files modifications

--- application/config/pd_seo.php
+++ application/config/pd_seo.php
@@ -8,24 +8,24 @@
 */
 
 //Home page
-$config['CP_home_index_title'] = "CodeigniterPlus Demo Home"; 
-$config['CP_home_index_key'] = "CodeigniterPlus"; 
-$config['CP_home_index_desc'] = "Experience the codeigniterplus project in live action and read the notes"; 
+$config['CP_home_index_title'] = "Africa Chronicles::Home"; 
+$config['CP_home_index_key'] = "Africa Chronicles"; 
+$config['CP_home_index_desc'] = "Everything Africa"; 
 
 //Error Page
 $config['CP_home_error_title'] = "Page Not Found"; 
-$config['CP_home_error_key'] = "CodeigniterPlus"; 
-$config['CP_home_error_desc'] = "Ultimate Codeigniter Enhancements";
+$config['CP_home_error_key'] = "Africa Chronicles"; 
+$config['CP_home_error_desc'] = "Ultimate Africa Chronicles Enhancements";
 
 //Contact Page
-$config['CP_home_contact_title'] = "CodeigniterPlus Demo Contact Page"; 
-$config['CP_home_contact_key'] = "CodeigniterPlus"; 
-$config['CP_home_contact_desc'] = "Experience the codeigniterplus contact page demo and notes";
+$config['CP_home_contact_title'] = "Africa Chronicles Contact Page"; 
+$config['CP_home_contact_key'] = "Africa Chronicles"; 
+$config['CP_home_contact_desc'] = "Experience the Africa Chronicles contact page and notes";
 
-$config['DX_auth_login_title'] = "CodeigniterPlus Demo Login Page"; 
-$config['DX_auth_login_key'] = "CodeigniterPlus"; 
-$config['DX_auth_login_desc'] = "Experience the codeigniterplus login page. Login with existing user/pass or via facebook/twitter"; 
+$config['DX_auth_login_title'] = "Africa Chronicles Login Page"; 
+$config['DX_auth_login_key'] = "Africa Chronicles"; 
+$config['DX_auth_login_desc'] = "Experience the Africa Chronicles login page. Login with existing user/pass or via facebook/twitter"; 
 
-$config['DX_auth_register_title'] = "CodeigniterPlus Demo Registration Page"; 
-$config['DX_auth_register_key'] = "CodeigniterPlus"; 
-$config['DX_auth_register_desc'] = "Experience the codeigniterplus registration page, test an real registration, either by form or via facebook/twitter"; 
+$config['DX_auth_register_title'] = "Africa Chronicles Registration Page"; 
+$config['DX_auth_register_key'] = "Africa Chronicles"; 
+$config['DX_auth_register_desc'] = "Experience the Africa Chronicles registration page, test an real registration, either by form or via facebook/twitter"; 
